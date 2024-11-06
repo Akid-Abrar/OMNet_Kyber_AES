f14597d1ee47d67dc6ab153fbb348b2b7fbcac69
veins <veins@instant-veins.car2x.org>
2024-11-06 18:44:40 +0000

    Ethernet V2

---
+++ src/Ethernet_Node.h
@@ -0,0 +1,28 @@
+#ifndef __KYBERAESPROJECT_NODE_H_
+#define __KYBERAESPROJECT_NODE_H_
+
+#include <omnetpp.h>
+#include <oqs/oqs.h>
+#include <openssl/aes.h>
+#include "messages_m.h"
+
+
+using namespace omnetpp;
+
+class Ethernet_Node : public cSimpleModule
+{
+  private:
+    uint8_t *publicKey = nullptr;
+    uint8_t *secretKey = nullptr;
+    uint8_t *sharedSecret = nullptr;
+    std::string formatHex(const uint8_t *data, size_t length);
+
+  protected:
+    virtual void initialize() override;
+    virtual void sendPublickKey();
+    virtual void sendEncryptedData();
+    virtual void handleMessage(cMessage *msg) override;
+    virtual void finish() override;
+};
+
+#endif
